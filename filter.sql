73856965c876d0a4ec9057c8a46b8043c6bfffbe
unknown <rizky.darmarazak@gmail.com>
2022-01-22 17:50:53 +0700

    query filter use where

---
+++ filter.sql
@@ -0,0 +1,33 @@
+-- FILTER USE WHERE
+-- AND OPERATOR (=, < , >, <> !=)
+select
+  country_id,
+  country_name,
+  region_id
+from countries
+where region_id > 1;
+
+-- LIKE (_ AND %);
+select 
+  country_id,
+  country_name,
+  region_id
+from countries
+where country_name LIKE '%A';
+
+-- OPERATOR IN
+select
+  country_id,
+  country_name,
+  region_id
+from countries
+where region_id IN (1,4);
+
+-- OPERATOR BETWEEN ... AND
+-- FOR FILTER BY RANGE
+select
+  employee_id,
+  first_name,
+  salary
+from employees
+where salary BETWEEN 4000 AND 100000;
